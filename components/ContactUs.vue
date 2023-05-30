c1b5a705e4cacf5612dc2b7941a48a981b58f43d
jiangzf <xiaojiangzf@163.com>
2023-05-30 17:27:50 +0800

    fix: 新增百度地址

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -31,7 +31,7 @@ const allAddressLists = [
       metroRoutes: '地鐵1號線“羅湖站”C出口； 地鐵9號線“人民南站” A1出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/476419e0bd27692f.png',
       googleMap: 'https://goo.gl/maps/yuWB8353yF3u4EJS6?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/b3/j3Yu'
     },
     {
       id: '102',
@@ -43,7 +43,7 @@ const allAddressLists = [
       metroRoutes: '地鐵1號線“大劇院”站F出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/faf033fd50b18824.png',
       googleMap: 'https://goo.gl/maps/HQsN5jNMTeZNbxuf7?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/a5/GtH'
     },
     {
       id: '103',
@@ -55,7 +55,7 @@ const allAddressLists = [
       metroRoutes: '地鐵1號線“羅湖站”D出口；地鐵9號線“人民南站”A1出口；',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/b741f54f1b105876.png',
       googleMap: 'https://goo.gl/maps/JMK7Huhaf9k88nbf8?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/24/Rxk'
     },
     {
       id: '104',
@@ -67,7 +67,7 @@ const allAddressLists = [
       metroRoutes: '地鐵1號線“羅湖站”D出口；地鐵9號線“人民南站”A1出口；',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/fe00a2ba47a161b5.png',
       googleMap: 'https://goo.gl/maps/nSmd9ZL66E4Yizp67?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/a9/Xgch'
     },
     {
       id: '105',
@@ -79,7 +79,7 @@ const allAddressLists = [
       metroRoutes: '地铁1号线“罗湖站”A出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/5f6257f23815c0b8.png',
       googleMap: 'https://goo.gl/maps/HefFWPuSqmdHbyBo9?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/d3/Awch'
     }
   ],
   // 福田區
@@ -94,7 +94,7 @@ const allAddressLists = [
       metroRoutes: '地鐵4號線和7號線“福民站”D出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/f75bb70e48d1a50e.png',
       googleMap: 'https://goo.gl/maps/evH9Z41S1o4fH3HR6?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/ea/Yhh'
     },
     {
       id: '202',
@@ -106,7 +106,7 @@ const allAddressLists = [
       metroRoutes: '地鐵4號線”福田口岸"站A出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/63dbe40ee13b53e9.png',
       googleMap: 'https://goo.gl/maps/mQgJXrTxvaaLB9Y1A?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/1f/kY0'
     },
   ],
   // 南山区
@@ -121,7 +121,7 @@ const allAddressLists = [
       metroRoutes: '地鐵9號線"南油西" 站 B出口；地鐵12號線“四海站”J出口平安銀行拐角處。',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/4783ff2e8f17beff.png',
       googleMap: 'https://goo.gl/maps/nK2iuEusGFcbqE216?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/2e/Pmif'
     },
     {
       id: '302',
@@ -133,7 +133,7 @@ const allAddressLists = [
       metroRoutes: '地鐵2號線和11號線"後海" 站 D1出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/d6c09b604319be78.png',
       googleMap: 'https://goo.gl/maps/WkeuvYNKWeLNhSzE7?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/36/4FH'
     },
     {
       id: '303',
@@ -145,7 +145,7 @@ const allAddressLists = [
       metroRoutes: '地鐵9號線"南山書城"站 D出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/47efa458657fb4a5.png',
       googleMap: 'https://goo.gl/maps/ud217ArM8ZGDJ8DJ8?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/b8/Mzj'
     },
   ],
   // 寶安區
@@ -160,7 +160,7 @@ const allAddressLists = [
       metroRoutes: '地鐵5號線“靈芝“站A出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/009aa648694461bb.png',
       googleMap: 'https://goo.gl/maps/ntfTf49VNaY9D9VL6?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/89/2LD'
     },
   ],
   // 龍華區
@@ -175,7 +175,7 @@ const allAddressLists = [
       metroRoutes: '地鐵4號線“紅山“站D出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/547f7fa32b8a7154.png',
       googleMap: 'https://goo.gl/maps/jtcrLmft7h8s3xfX6?coh=178572&entry=tt',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/3a/nbch'
     },
     {
       id: '502',
@@ -187,7 +187,7 @@ const allAddressLists = [
       metroRoutes: '地鐵4號線“龍華站”A出口',
       addressUrl: 'https://static.cmereye.com/imgs/2023/05/2d4f2be9d307d63a.png',
       googleMap: 'https://goo.gl/maps/VKT1orRbiS8mAiN39?coh=178572&entry=tt門診門',
-      baiduMap: ''
+      baiduMap: 'https://j.map.baidu.com/c5/AZ4c'
     },
   ]
 ]
@@ -265,10 +265,10 @@ const handleAddress = (_idx: string) => {
             <span>{{addressItem.metroRoutes || ''}}</span>
           </div>
           <div class="mapBtn">
-            <a :href="addressItem.googleMap">
+            <a :href="addressItem.googleMap" target="_blank">
             <div class="mapBtn-in">Google地圖</div>
             </a>
-            <a :href="addressItem.baiduMap">
+            <a :href="addressItem.baiduMap" target="_blank">
               <div class="mapBtn-in">百度地圖</div>
             </a>
           </div>
