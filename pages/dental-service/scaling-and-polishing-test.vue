37cf0729d428d6211c9e4aeab0e34a23f3864b72
jiangzf <xiaojiangzf@163.com>
2023-10-18 16:35:31 +0800

    fix: page 更新

--- pages/dental-service/scaling-and-polishing-test.vue
+++ pages/dental-service/scaling-and-polishing-test.vue
@@ -840,8 +840,8 @@ const careData = {
               top: 50%;
               left: 50%;
               transform: translate(-50%, -50%);
-              max-width: 90%;
-              max-height: 90%;
+              max-width: 70%;
+              max-height: 70%;
             }
           }
         }
