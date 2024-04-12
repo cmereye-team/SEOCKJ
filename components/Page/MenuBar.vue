4719be413f988042c2b712594420e5a1f493e039
jiangzf <xiaojiangzf@163.com>
2024-04-12 15:24:16 +0800

    fix: 首页顶部

--- components/Page/MenuBar.vue
+++ components/Page/MenuBar.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { defineProps } from "vue";
 import { useElementBounding } from '@vueuse/core'
+import { smallPhoneNum,phoneNum } from '~/assets/js/common' 
 const menuBar = ref(null)
 const { x, y, top, right, bottom, left, width, height } = useElementBounding(menuBar)
 defineProps({
@@ -13,8 +14,29 @@ defineProps({
 </script>
 
 <template>
-  <div class="menuBar" ref="menuBar">
-    {{top}}
+  <div class="menuBar " ref="menuBar">
+    <div class="menuBar-in">
+      <div class="menuBar-in-l">
+        <div class="menuBar-box">
+          <img src="@/assets/images/icon_38.svg" alt="">
+          <p>深圳市醫保定點醫院</p>
+        </div>
+        <div class="menuBar-box">
+          <img src="@/assets/images/icon_39.svg" alt="">
+          <p>深圳二級口腔醫院</p>
+        </div>
+      </div>
+      <div class="menuBar-in-r">
+        <nuxtLink :to="`tel: ${phoneNum}`" class="menuBar-box">
+          <img src="@/assets/images/navIcon_1.png" alt="">
+          <p>香港熱線：(852) {{smallPhoneNum}}</p>
+        </nuxtLink>
+        <div class="menuBar-box">
+          <p>營業時間：9:00-17:30（假日照常應診）</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,10 +45,50 @@ defineProps({
 .menuBar{
   width: 100%;
   height: 67px;
-  // position: fixed;
-  // top: 0;
-  // left: 0;
   background: var(--indexColor1);
+  display: flex;
+  padding: 0 80px;
+  &-in{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .menuBar-box{
+      display: flex;
+      align-items: center;
+      &>img{
+        display: inline-block;
+        vertical-align: middle;
+        margin-top: -5px;
+        margin-right: 5px;
+      }
+      p{
+        font-size: 22px;
+        color: #fff;
+        display: inline-block;
+      }
+    }
+    &-l{
+      display: flex;
+      align-items: center;
+      .menuBar-box{
+        margin-right: 57px;
+      }
+    }
+    
+    &-r{
+      display: flex;
+      align-items: center;
+      .menuBar-box{
+        margin-left: 40px;
+        &>img{
+          max-height: 24px;
+          margin-top: -3px;
+        }
+      }
+    }
+    
+  }
 }
 @media (min-width: 768px) and (max-width: 1452px) {}
 @media screen and (max-width: 768px) {}
