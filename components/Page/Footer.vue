93f454b8e959b1736b501b0fadb826acce09b6d2
jiangzf <xiaojiangzf@163.com>
2024-04-15 15:25:37 +0800

    fix: 暂存footer

--- components/Page/Footer.vue
+++ components/Page/Footer.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-
 import { useAppState } from '~/stores/appState'
-import { smallPhoneNum,phoneNum } from '~/assets/js/common' 
+import { smallPhoneNum, phoneNum } from '~/assets/js/common'
+import serviceLists from '~/assets/js/service'
 const appState = useAppState()
 const navLists = [
   {
@@ -22,7 +22,7 @@ const navLists = [
   },
   {
     name: 'components.header.menuLists.federation-of-trade-unions-zone.name',
-    link: `/federation-of-trade-unions-zone`
+    link: `/federation-of-trade-unions-zone`,
   },
   {
     name: 'components.header.menuLists.menu_news.name',
@@ -45,95 +45,154 @@ const navLists = [
 let _bool = ref(false)
 
 const centerDialogVisible = ref(false)
-const handlecopywechatcode = () =>{
-  if (navigator.clipboard) {  
-      navigator.clipboard.writeText('ckjhongkong').then(function() {  
+const handlecopywechatcode = () => {
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText('ckjhongkong').then(
+      function () {
         _bool.value = true
-      }, function(err) {
-          ElMessage({
-            showClose: true,
-            message: '操作異常，請刷新頁面試試',
-            type: 'warning',
-          })
-      });  
-  } else {  
-      alert('Clipboard API is not supported by your browser.');  
-  }  
+      },
+      function (err) {
+        ElMessage({
+          showClose: true,
+          message: '操作異常，請刷新頁面試試',
+          type: 'warning',
+        })
+      }
+    )
+  } else {
+    alert('Clipboard API is not supported by your browser.')
+  }
 }
 
-const handleopenwechat = () =>{
-  window.location.href = "weixin://"
+const handleopenwechat = () => {
+  window.location.href = 'weixin://'
 }
-
 </script>
 
 <template>
-  <div class="bgColor">
-    <div class="footer-content bigPageCon">
-      <div class="footer-content-nav">
+  <div class="bgColor footerBox">
+    <!-- <div class="footer-l"> -->
+      <div class="footer-content bigPageCon">
+        <div class="footer-content-nav">
         <div v-for="(navItem, navIndex) in navLists" :key="navIndex">
           <nuxt-link :to="navItem.link" :title="$t(navItem.name)">
             {{ $t(navItem.name) }}
           </nuxt-link>
         </div>
       </div>
-      <div class="footer-content-logo_one">
-        <nuxt-link :to="'/'" title="深圳愛康健口腔醫院" alt="深圳愛康健口腔醫院">
-          <img src="@/assets/images/logo_2.svg" alt="logo" />
-        </nuxt-link>
-      </div>
-      <div class="footer-content-text">
-        {{$t('components.footer.text1')}} {{'\n'}}{{$t('components.footer.text2')}}
-        <nuxt-link :to="`tel: ${phoneNum}`" class="footer-content-text-tel">
-          <img src="@/assets/images/navIcon_1.png" alt="">
-          <span>香港熱線：(852) {{smallPhoneNum}}</span>
-        </nuxt-link>
-      </div>
-      <div class="footer-content-icon">
-        <div class="footer-content-icon-in">
-          <nuxt-link to="https://www.facebook.com/ckjdental.hk/" target="_blank">
-            <img src="@/assets/images/icon_1.png" alt="facebook" title="facebook" />
+        <div class="footer-content-logo_one">
+          <nuxt-link
+            :to="'/'"
+            title="深圳愛康健口腔醫院"
+            alt="深圳愛康健口腔醫院"
+          >
+            <img src="@/assets/images/footerLogo.svg" alt="logo" />
           </nuxt-link>
         </div>
-        <div class="footer-content-icon-in">
-          <nuxt-link to="https://www.instagram.com/ckj_hk/" target="_blank">
-            <img src="@/assets/images/icon_2.png" alt="instagram" title="instagram" />
+        <div class="footer-content-text">
+          {{ $t('components.footer.text1') }} {{ '\n'
+          }}{{ $t('components.footer.text2') }}
+          <nuxt-link :to="`tel: ${phoneNum}`" class="footer-content-text-tel">
+            <img src="@/assets/images/navIcon_1.png" alt="" />
+            <span>香港熱線：(852) {{ smallPhoneNum }}</span>
           </nuxt-link>
         </div>
-        <div class="footer-content-icon-in">
-          <nuxt-link to="https://www.youtube.com/channel/UC4AQD5eeOiHIGd3QYFGK4aA" target="_blank">
-            <img src="@/assets/images/icon_3.png" alt="youtube" title="youtube" />
-          </nuxt-link>
-        </div>
-        <div class="footer-content-icon-in">
-          <div class="weChat">
-            <img src="@/assets/images/navIcon_3.png" alt="weChat" />
-            <div class="navbarBox">
-              <div class="navbarBox-in">
-                <div class="weChat"><img src="https://static.cmereye.com/imgs/2023/09/a43a869a1fc07eea.jpg" alt="二维码"></div>
+        <div class="footer-content-icon">
+          <div class="footer-content-icon-in">
+            <nuxt-link
+              to="https://www.facebook.com/ckjdental.hk/"
+              target="_blank"
+            >
+              <img
+                src="@/assets/images/icon_1.png"
+                alt="facebook"
+                title="facebook"
+              />
+            </nuxt-link>
+          </div>
+          <div class="footer-content-icon-in">
+            <nuxt-link to="https://www.instagram.com/ckj_hk/" target="_blank">
+              <img
+                src="@/assets/images/icon_2.png"
+                alt="instagram"
+                title="instagram"
+              />
+            </nuxt-link>
+          </div>
+          <div class="footer-content-icon-in">
+            <nuxt-link
+              to="https://www.youtube.com/channel/UC4AQD5eeOiHIGd3QYFGK4aA"
+              target="_blank"
+            >
+              <img
+                src="@/assets/images/icon_3.png"
+                alt="youtube"
+                title="youtube"
+              />
+            </nuxt-link>
+          </div>
+          <div class="footer-content-icon-in">
+            <div class="weChat">
+              <img src="@/assets/images/navIcon_3.png" alt="weChat" />
+              <div class="navbarBox">
+                <div class="navbarBox-in">
+                  <div class="weChat">
+                    <img
+                      src="https://static.cmereye.com/imgs/2023/09/a43a869a1fc07eea.jpg"
+                      alt="二维码"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="weChat-mb" @click="handlecopywechatcode()">
-            <img src="@/assets/images/navIcon_3.png" alt="weChat" />
+            <div class="weChat-mb" @click="handlecopywechatcode()">
+              <img src="@/assets/images/navIcon_3.png" alt="weChat" />
+            </div>
           </div>
         </div>
+        <div class="footer-content-Disclaimer">
+          <nuxt-link to="/privacyPolicy">{{
+            $t('components.footer.privacy_policy')
+          }}</nuxt-link>
+          &
+          <nuxt-link to="/disclaimer">{{
+            $t('components.footer.disclaimer')
+          }}</nuxt-link>
+        </div>
+        <div class="footer-content-copyright">
+          ©2024 {{ $t('components.footer.all_rights_reserved') }}
+        </div>
+        <div class="footer-content-copyright">
+          本網站資訊僅供參考，不能作診療及醫療為依據
+        </div>
       </div>
-      <div class="footer-content-Disclaimer">
-        <nuxt-link to="/privacyPolicy">{{$t('components.footer.privacy_policy')}}</nuxt-link>
-        &
-        <nuxt-link to="/disclaimer">{{$t('components.footer.disclaimer')}}</nuxt-link>
-      </div>
-      <div class="footer-content-copyright">©2024 {{$t('components.footer.all_rights_reserved')}}</div>
-      <div class="footer-content-copyright">
-        本網站資訊僅供參考，不能作診療及醫療為依據
+    <!-- </div>
+    <div class="footer-c">
+      <div class="footer-c-nav">
+        <div v-for="(navItem, navIndex) in navLists" :key="navIndex">
+          <nuxt-link :to="navItem.link" :title="$t(navItem.name)">
+            {{ $t(navItem.name) }}
+          </nuxt-link>
+        </div>
       </div>
     </div>
-    <div :class="['dialogBox',{show:_bool}]" @click="_bool=false">
-      <div :class="['dialogBox-in',{'show-in':_bool}]" @click.stop="">
-        <div class="title">
-          WeChat ID已複製
+    <div class="footer-r">
+      <nuxt-link :to="`tel: ${phoneNum}`" class="footer-content-text-tel">
+        <img src="@/assets/images/navIcon_1.png" alt="" />
+        <span>香港熱線：(852) {{ smallPhoneNum }}</span>
+      </nuxt-link>
+      <div class="serviceCar">
+        <div class="serviceCar-title">牙科服務</div>
+        <div class="serviceCar-content">
+          <div class="serviceCar-content-item" v-for="(item,index) in serviceLists" :key="index">
+            <nuxt-link :to="item.link"><span>{{$t(item.name)}}</span></nuxt-link>
+          </div>
         </div>
+      </div>
+    </div> -->
+    <div :class="['dialogBox', { show: _bool }]" @click="_bool = false">
+      <div :class="['dialogBox-in', { 'show-in': _bool }]" @click.stop="">
+        <div class="title">WeChat ID已複製</div>
         <div class="content">
           點擊「打開微信」進入微信，點右上⊕，粘貼ID，添加客服開始免費咨詢！
         </div>
@@ -150,41 +209,41 @@ const handleopenwechat = () =>{
 
 <style lang="scss" scoped>
 @keyframes contentIn {
-  to{
+  to {
     opacity: 1;
   }
 }
 @keyframes topIn {
-  50%{
+  50% {
     top: 52%;
   }
-  75%{
+  75% {
     top: 49%;
   }
-  100%{
+  100% {
     top: 50%;
     opacity: 1;
   }
 }
-.dialogBox{
+.dialogBox {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
   z-index: 999;
-  background: rgba(0,0,0,.3);
+  background: rgba(0, 0, 0, 0.3);
   opacity: 0;
   display: none;
-  &.show{
+  &.show {
     display: block;
     animation: contentIn 1s forwards;
   }
-  &-in{
+  &-in {
     position: absolute;
     left: 50%;
     top: 0;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     width: calc(100% - 60px);
     height: auto;
     max-width: 768px;
@@ -196,32 +255,88 @@ const handleopenwechat = () =>{
     background: #fff;
     padding: 10px 30px 15px;
     border-radius: 10px;
-    &.show-in{
-      animation: topIn .7s ease-out forwards;
+    &.show-in {
+      animation: topIn 0.7s ease-out forwards;
     }
-    .title{
+    .title {
       font-size: 20px;
       font-weight: 600;
       text-align: center;
     }
-    .content{
+    .content {
       font-size: 16px;
       margin: 10px 0 20px;
     }
-    .btn{
+    .btn {
       display: flex;
       justify-content: center;
     }
   }
 }
 @keyframes animBottomIn {
-  from{
+  from {
   }
-  to{
+  to {
     opacity: 1;
     transform: translateY(0);
   }
 }
+// .footerBox{
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   .footer-l{
+
+//   }
+//   .footer-c{
+//     display: flex;
+//     align-items: center;
+//     height: 100%;
+//     margin: 0 97px;
+//     &-nav{
+//       display: flex;
+//       flex-direction: column;
+//       font-size: 22px;
+//       line-height: 160%; 
+//       color: #fff;
+//       &>div{
+//         margin: 10px 0;
+//       }
+//     }
+//   }
+//   .footer-r{
+//     .footer-content-text-tel{
+//         margin-top: 25px;
+//         display: flex;
+//         align-items: center;
+//         color: #fff;
+//         font-size: 22px;
+//         & > img {
+//           width: 26px;
+//           height: 24px;
+//           margin-right: 13px;
+//         }
+//     }
+//     .serviceCar{
+//       &-title{
+//         font-size: 22px;
+//         line-height: 160%;
+//         margin: 29px 0 0;
+//         color: #fff;
+//       }
+//       &-content{
+//         &-item{
+//           margin-top: 23px;
+//           margin-right: 47px;
+//           font-style: normal;
+//           line-height: 160%; /* 35.2px */
+//           letter-spacing: 5.5px;
+//           color: #fff;
+//         }
+//       }
+//     }
+//   }
+// }
 .footer-content {
   display: flex;
   flex-direction: column;
@@ -246,8 +361,8 @@ const handleopenwechat = () =>{
   }
   &-logo_one {
     padding: 25px 0;
-    & > img {
-      width: 150px;
+    img {
+      width: 127px;
     }
   }
   &-text {
@@ -257,11 +372,11 @@ const handleopenwechat = () =>{
     line-height: 160%;
     text-shadow: 0px 0px 4px rgba(255, 120, 117, 0.45);
     white-space: pre-wrap;
-    &-tel{
+    &-tel {
       margin-top: 25px;
       display: flex;
       align-items: center;
-      &>img{
+      & > img {
         width: 26px;
         height: 24px;
         margin-right: 13px;
@@ -276,29 +391,29 @@ const handleopenwechat = () =>{
       display: flex;
       align-items: center;
       cursor: pointer;
-      
+
       img {
         width: 100%;
       }
-      .weChat{
+      .weChat {
         width: 35px;
         position: relative;
-        img{
+        img {
           width: 100%;
         }
-        &:hover .navbarBox{
+        &:hover .navbarBox {
           display: flex;
           animation: animBottomIn 1s forwards;
         }
-        .navbarBox{
+        .navbarBox {
           position: absolute;
           bottom: 100%;
           padding-right: 10px;
           display: none;
           opacity: 0;
           transform: translateY(20px);
-          &-in{
-            transition: all .5s;
+          &-in {
+            transition: all 0.5s;
             background: var(--indexColor1);
             height: 100%;
             padding: 20px;
@@ -306,37 +421,37 @@ const handleopenwechat = () =>{
             align-items: center;
             border-radius: 10px;
             color: #fff;
-            .weChat{
+            .weChat {
               width: 200px;
               background: var(--indexColor3);
-              img{
+              img {
                 width: 100%;
               }
             }
           }
         }
       }
-      .weChat-mb{
+      .weChat-mb {
         display: none;
       }
     }
   }
-  &-copyright{
+  &-copyright {
     color: #fff;
     font-size: 16px;
     text-align: center;
     margin-top: 5px;
   }
-  &-Disclaimer{
+  &-Disclaimer {
     color: #fff;
     font-size: 16px;
     text-align: center;
     margin-top: 30px;
-    a{
-      transition: all .3s;
+    a {
+      transition: all 0.3s;
       display: inline-block;
       border-bottom: 1px solid var(--indexColor);
-      &:hover{
+      &:hover {
         border-bottom: 1px solid #fff;
       }
     }
@@ -347,40 +462,40 @@ const handleopenwechat = () =>{
     padding: 3.125vw;
     &-nav {
       & > div {
-        padding: .5208vw 1.0417vw;
+        padding: 0.5208vw 1.0417vw;
         font-size: 1.1458vw;
-        text-shadow: 0px 0px .2083vw rgba(255, 120, 117, 0.45);
+        text-shadow: 0px 0px 0.2083vw rgba(255, 120, 117, 0.45);
       }
     }
     &-logo_one {
       padding: 1.3021vw 0;
-      & > img {
-        width: 7.8125vw;
+      img {
+        width: 6.6146vw;
       }
     }
     &-text {
       font-size: 1.1458vw;
-      &-tel{
+      &-tel {
         margin-top: 1.3021vw;
-        &>img{
+        & > img {
           width: 1.3542vw;
           height: 1.25vw;
-          margin-right: .6771vw;
+          margin-right: 0.6771vw;
         }
       }
     }
     &-icon {
       margin-top: 1.5625vw;
       &-in {
-        padding: 0 .651vw;
-        .weChat{
-          .navbarBox{
-            padding-right: .5208vw;
+        padding: 0 0.651vw;
+        .weChat {
+          .navbarBox {
+            padding-right: 0.5208vw;
             transform: translateY(1.0417vw);
-            &-in{
+            &-in {
               padding: 1.0417vw;
-              border-radius: .5208vw;
-              .weChat{
+              border-radius: 0.5208vw;
+              .weChat {
                 width: 10.4167vw;
               }
             }
@@ -388,12 +503,12 @@ const handleopenwechat = () =>{
         }
       }
     }
-    &-copyright{
-      font-size: .8333vw;
-      margin-top: .2604vw;
+    &-copyright {
+      font-size: 0.8333vw;
+      margin-top: 0.2604vw;
     }
-    &-Disclaimer{
-      font-size: .8333vw;
+    &-Disclaimer {
+      font-size: 0.8333vw;
       margin-top: 1.0417vw;
     }
   }
@@ -411,20 +526,20 @@ const handleopenwechat = () =>{
     }
     &-icon {
       margin: 30px 0;
-      &-in{
-        .weChat{
+      &-in {
+        .weChat {
           display: none;
         }
-        .weChat-mb{
+        .weChat-mb {
           width: 35px;
           display: block;
-          img{
+          img {
             width: 100%;
           }
         }
       }
     }
-    &-Disclaimer{
+    &-Disclaimer {
       margin-top: 0;
     }
   }
