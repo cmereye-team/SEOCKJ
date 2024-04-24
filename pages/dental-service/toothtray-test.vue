abdb04b3314e32e3589cfc8cf3344aeaabfb2fe4
jiangzf <xiaojiangzf@163.com>
2024-04-24 10:39:36 +0800

    fix: 添加牙托页面

--- pages/dental-service/toothtray-test.vue
+++ pages/dental-service/toothtray-test.vue
@@ -373,7 +373,51 @@ const processData:any = {
     },
   ]
 }
-
+const advantageData = {
+  title: '為什麼選擇愛康健的定製牙托？',
+  lists: [
+    {
+      img: 'https://static.cmereye.com/static/ckj/imgs/toothtray/advantage1.svg',
+      name: '專業製作',
+      text: '我們的牙托由資深矯正科醫生根據患者的牙齒模型精確製作，確保每一個牙托都能完美貼合。'
+    },
+    {
+      img: 'https://static.cmereye.com/static/ckj/imgs/toothtray/advantage2.svg',
+      name: '高品質材料',
+      text: '愛康健使用高品質材料製作牙托，確保牙托耐用且對身體安全無害。'
+    },
+    {
+      img: 'https://static.cmereye.com/static/ckj/imgs/toothtray/advantage3.svg',
+      name: '個性化設計',
+      text: '醫生根據個人口腔狀況和需求獨立定製，更符合患者個人的使用習慣和舒適度。'
+    },
+    {
+      img: 'https://static.cmereye.com/static/ckj/imgs/toothtray/advantage4.svg',
+      name: '專業保養\n及緊急維修服務',
+      text: '我們提供專業牙托保養及緊急維修服務，讓牙托延長其使用壽命。'
+    },
+  ]
+}
+const caseLists = [
+  {
+    img: 'https://static.cmereye.com/static/ckj/imgs/toothtray/Ellipse203.png',
+    name: '明叔',
+    jos: '小巴司機',
+    context: '年紀大有幾隻牙磨損特別嚴重。選擇咗全瓷牙套，現在笑口常開，食嘢無難度。'
+  },
+  {
+    img: 'https://static.cmereye.com/static/ckj/imgs/toothtray/Ellipse202.png',
+    name: 'May',
+    jos: '準奶奶',
+    context: '屋企辦喜事，補返隻缺牙見親戚朋友都有體面啲，影相都靚啲。'
+  },
+  {
+    img: 'https://static.cmereye.com/static/ckj/imgs/toothtray/Ellipse204.png',
+    name: '黃伯伯',
+    jos: '長壽長者',
+    context: '以前都整過假牙，但戴到好唔舒服。愛康健醫生手勢好好多，排牙好貼服，掂呀。'
+  },
+]
 </script>
 
 <template>
@@ -487,6 +531,51 @@ const processData:any = {
         </div>
       </div>
     </div>
+    <div class="advantage">
+      <div class="advantage-title dentistryServices-title">
+        <div class="dentistryServices-title-in bb">隱形牙箍四大優點</div>
+      </div>
+      <div class="advantage-lists">
+        <div class="advantage-lists-in" v-for="(advantageItem,advantageIndex) in advantageData.lists" :key="advantageIndex">
+          <div class="advantage-lists-in-image">
+            <img :src="advantageItem.img" :alt="advantageItem.name" :title="advantageItem.name">
+          </div>
+          <div class="advantage-lists-in-name">{{advantageItem.name}}</div>
+          <div class="advantage-lists-in-text">{{advantageItem.text}}</div>
+        </div>
+      </div>
+    </div>
+    <div class="case">
+      <div class="case-title">
+        <div class="case-title-in">個案分享</div>
+      </div>
+      <div class="case-context">
+        <span>
+          解決缺牙問題
+        </span>
+        <span>
+          我揀愛康健！
+        </span>
+      </div>
+      <div class="case-content">
+        <div class="case-content-in">
+          <div v-for="(caseItem,caseIndex) in caseLists" :key="caseIndex">
+            <div class="case-content-in-l">
+              <div>
+                <img :src="caseItem.img" alt="">
+              </div>
+              <div>
+                <span>{{caseItem.jos}}</span>
+                <span>{{caseItem.name}}</span>
+              </div>
+            </div>
+            <div class="case-content-in-r">
+              {{caseItem.context}}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <ServiceProblem :problemData="problemData" />
     <serviceCard />
     <NewAddress />
@@ -546,7 +635,7 @@ const processData:any = {
   &-context{
     text-align: center;
     font-style: normal;
-    font-weight: 600;
+    font-weight: 400;
     font-size: 20px;
     line-height: 160%;
     color: #666666;
@@ -556,7 +645,7 @@ const processData:any = {
   &-tableRemark{
     margin-top: 28px;
     font-style: normal;
-    font-weight: 600;
+    font-weight: 500;
     font-size: 20px;
     line-height: 160%;
     color: var(--indexColor1);
@@ -584,8 +673,8 @@ const processData:any = {
             flex-direction: column;
             justify-content: center;
             font-style: normal;
-            font-weight: 600;
-            font-size: 24px;
+            font-weight: 400;
+            font-size: 20px;
             line-height: 160%;
             color: #666666;
             padding: 0 18px;
@@ -616,8 +705,8 @@ const processData:any = {
           .th{
             background: var(--indexColor1);
             font-style: normal;
-            font-weight: 700;
-            font-size: 28px;
+            font-weight: 500;
+            font-size: 24px;
             line-height: 160%;
             color: #FFFFFF;
             margin-bottom: 6px;
@@ -632,8 +721,8 @@ const processData:any = {
             width: 100%;
             .td{
               font-style: normal;
-              font-weight: 700;
-              font-size: 28px;
+              font-weight: 500;
+              font-size: 24px;
               line-height: 160%;
               color: var(--indexColor1);
               text-shadow:
@@ -656,16 +745,16 @@ const processData:any = {
           }
         } 
         &:nth-of-type(2){
-          height: 183px;
+          height: 140px;
         } 
         &:nth-of-type(3){
-          height: 144px;
+          height: 125px;
         } 
         &:nth-of-type(4){
-          height: 109px;
+          height: 69px;
         } 
         &:nth-of-type(5){
-          height: 182px;
+          height: 140px;
         }
         &:nth-of-type(6){
           height: 105px;
@@ -686,8 +775,8 @@ const processData:any = {
           .contrast-table-in{
             &>div{
               font-style: normal;
-              font-weight: 700;
-              font-size: 28px;
+              font-weight: 500;
+              font-size: 24px;
               line-height: 160%;
               background: var(--indexColor2);
               color: #505050;
@@ -719,6 +808,8 @@ const processData:any = {
     width: 100%;
     max-width: 1091px;
     margin: 38px auto 0;
+    overflow: hidden;
+    border-radius: 65px;
   }
   .table{
     display: flex;
@@ -774,7 +865,154 @@ const processData:any = {
     }
   }
 }
-
+.advantage{
+  margin-top: 117px;
+  &-title{
+    margin-bottom: 54px;
+  }
+  &-lists{
+    width: 100%;
+    max-width: 1325px;
+    margin: 0 auto;
+    display: flex;
+    &-in{
+      flex: 1;
+      margin: 0 calc((40 / 1325) * 100%);
+      &-image{
+        width: auto;
+        margin: 0 calc((22 / 230) * 100%);
+        border-radius: 10px;
+        height: 0;
+        padding-bottom: calc((186 / 230) * 100%);
+        position: relative;
+        img{
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%,-50%);
+          max-width: 90%;
+          max-height: 75%;
+        }
+      }
+      &-name{
+        color: var(--indexColor1);
+        text-align: center;
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 160%;
+        margin: 10px 0;
+      }
+      &-text{
+        color: var(--textColor);
+        text-align: justify;
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 160%; 
+        padding: 0 10px;
+        letter-spacing: 3.8px;
+      }
+    }
+  }
+}
+.case{
+  width: 100%;
+  margin-top: 97px;
+  &-title{
+    display: flex;
+    justify-content: center;
+    &-in{
+      font-style: normal;
+      position: relative;
+      font-weight: 600;
+      font-size: 30px;
+      line-height: 150%;
+      text-indent: 65px;
+      color: #666666;
+      background: url(@/assets/images/indexTitle_bg_2.png) no-repeat;
+      background-size: auto 100%;
+      padding-bottom: 5px;
+      padding-right: 7px;
+    }
+  }
+  &-context{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 21px;
+    span{
+      color: #FC1682;
+      text-align: center;
+      font-size: 35px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 160%; 
+    }
+  }
+  &-content{
+    display: flex;
+    justify-content: center;
+    margin-top: 46px;
+    padding: 58px 0 62px;
+    background: linear-gradient(279deg, rgba(252, 22, 130, 0.40) -0.55%, rgba(252, 22, 130, 0.28) -0.54%, rgba(255, 168, 198, 0.00) 99.3%);
+    &-in{
+      width: 100%;
+      max-width: 1560px;
+      margin: 0 auto;
+      display: flex;
+      justify-content: center;
+      
+      &>div{
+        display: flex;
+        width: calc(100% / 3);
+        flex-direction: column;
+        background: #fff;
+        padding: 14px 29px 25px;
+        box-sizing: border-box;
+        &:not(:first-child){
+          margin-left: 16px;
+        }
+      }
+      
+      &-l{
+        display: flex;
+        img{
+          border-radius: 50%;
+          margin-top: -36%;
+          border: 8px solid #fff;
+        }
+        span{
+          display: block;
+          margin-left: 18px;
+          &:nth-of-type(1){
+            color: #4D4D4D;
+            font-size: 28px;
+            font-style: normal;
+            font-weight: 600;
+            line-height: 160%;
+          }
+          &:nth-of-type(2){
+            color: #FC1682;
+            font-size: 21px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: 160%;
+          }
+        }
+      }
+      &-r{
+        margin-top: 15px;
+        color: #4D4D4D;
+        text-align: justify;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 160%;
+      }
+    }
+  }
+}
 @media (min-width: 768px) and (max-width: 1920px) {
   .reason{
     max-width: 46.5625vw;
@@ -822,7 +1060,7 @@ const processData:any = {
           &.content{
             margin-bottom: .3125vw;
             &>div{
-              font-size: 1.25vw;
+              font-size: 1.0417vw;
               padding: 0 .9375vw;
               &>div{
                 span{
@@ -840,13 +1078,13 @@ const processData:any = {
             height: 14.1146vw;
             margin-bottom: .3125vw;
             .th{
-              font-size: 1.4583vw;
+              font-size: 1.25vw;
               margin-bottom: .3125vw;
               height: 3.5938vw;
             }
             &>div{
               .td{
-                font-size: 1.4583vw;
+                font-size: 1.25vw;
               }
               span{
                 &:last-child{
@@ -856,16 +1094,16 @@ const processData:any = {
             }
           } 
           &:nth-of-type(2){
-            height: 9.5313vw;
+            height: 7.2917vw;
           } 
           &:nth-of-type(3){
-            height: 7.5vw;
+            height: 6.5104vw;
           } 
           &:nth-of-type(4){
-            height: 5.6771vw;
+            height: 3.5938vw;
           } 
           &:nth-of-type(5){
-            height: 9.4792vw;
+            height: 7.2917vw;
           }
           &:nth-of-type(6){
             height: 5.4688vw;
@@ -882,7 +1120,7 @@ const processData:any = {
             margin-right: .2083vw;
             .contrast-table-in{
               &>div{
-                font-size: 1.4583vw;
+                font-size: 1.25vw;
                 margin-bottom: .3125vw;
               }
             }
@@ -891,6 +1129,109 @@ const processData:any = {
             padding-left: .2083vw;
             border-radius: 0 3.125vw 3.125vw 0;
           } 
+        }
+      }
+    }
+  }
+  .process{
+    margin-top: 4.1667vw;
+    &-in{
+      max-width: 56.8229vw;
+      margin: 1.9792vw auto 0;
+      border-radius: 3.3854vw;
+    }
+    .table{
+      &-in{
+        min-height: 3.6979vw;
+        .context{
+          font-size: .9896vw;
+          &:not(:last-child){
+            margin-bottom: .5208vw;
+          }
+        }
+        h3{
+          font-size: 1.1979vw;
+        }
+        &.text{
+          padding: 1.5625vw 2.8646vw;
+        }
+        &:not(:last-child){
+          margin-right: .4167vw;
+        }
+      }
+      &:not(:last-child){
+        margin-bottom: .4167vw;
+      }
+    }
+  }
+  .advantage{
+    margin-top: 6.0938vw;
+    &-title{
+      margin-bottom: 2.8125vw;
+    }
+    &-lists{
+      max-width: 69.0104vw;
+      &-in{
+        &-image{
+          border-radius: .5208vw;
+        }
+        &-name{
+          font-size: 1.1458vw;
+          margin: .5208vw 0;
+        }
+        &-text{
+          font-size: .9375vw;
+          padding: 0 .5208vw;
+          letter-spacing: .1979vw;
+        }
+      }
+    }
+  }
+  .case{
+    margin-top: 5.0521vw;
+    &-title{
+      &-in{
+        font-size: 1.5625vw;
+        text-indent: 3.3854vw;
+        padding-bottom: .2604vw;
+        padding-right: .3646vw;
+      }
+    }
+    &-context{
+      margin-top: 1.0938vw;
+      span{
+        font-size: 1.8229vw;
+      }
+    }
+    &-content{
+      margin-top: 2.3958vw;
+      padding: 3.0208vw 0 3.2292vw;
+      &-in{
+        max-width: 81.25vw;
+        &>div{
+          padding: .7292vw 1.5104vw 1.3021vw;
+          &:not(:first-child){
+            margin-left: .8333vw;
+          }
+        }
+        &-l{
+          img{
+            width: 9.1146vw;
+            border: .4167vw solid #fff;
+          }
+          span{
+            margin-left: .9375vw;
+            &:nth-of-type(1){
+              font-size: 1.4583vw;
+            }
+            &:nth-of-type(2){
+              font-size: 1.0938vw;
+            }
+          }
+        }
+        &-r{
+          margin-top: .7813vw;
+          font-size: 1.0417vw;
         }
       }
     }
@@ -1009,6 +1350,140 @@ const processData:any = {
       }
       :deep(.swiper-scrollbar-drag){
         background: var(--indexColor1);
+      }
+    }
+  }
+  .process{
+    margin-top: 50px;
+    padding: 0 30px;
+    &-in{
+      margin-top: 30px;
+      border-radius: 0 0 25px 25px;
+    }
+    .table{
+      &-in{
+        min-height: 34px;
+        .context{
+          font-size: 12px;
+        }
+        &.text{
+          padding: 20px 12px;
+        }
+        &.h2{
+          border-radius: 25px 25px 0 0;
+        }
+        h3{
+          font-size: 16px;
+        }
+        &:not(:last-child){
+          margin-right: 4px;
+        }
+      }
+      &:not(:last-child){
+        margin-bottom: 4px;
+      }
+    }
+  }
+  .advantage{
+    margin-top: 60px;
+    &-title{
+      margin-bottom: 0;
+    }
+    &-lists{
+      max-width: 390px;
+      padding: 0 15px;
+      box-sizing: border-box;
+      flex-wrap: wrap;
+      &-in{
+        width: 50%;
+        flex: initial;
+        width: calc((100% - 60px) / 2);
+        margin: 30px 15px 0;
+        &-image{
+          margin: 0 10px;
+          padding-bottom: calc(100% - 20px);
+          span{
+            top: 14px;
+            left: 18px;
+          }
+          img{
+            width: 100%;
+          }
+        }
+        &-name{
+          margin: 0 0 10px;
+          font-size: 20px;
+          white-space: pre-wrap;
+        }
+        &-text{
+          font-size: 15px;
+          padding: 0;
+          letter-spacing: 3px;
+        }
+        &:nth-child(n+3){
+          .advantage-lists-in-name{
+            min-height: 64px;
+            display: flex;
+            align-items: flex-end;
+            justify-content: center;
+          }
+        }
+      }
+    }
+  }
+  .case{
+    margin-top: 60px;
+    background: linear-gradient(279deg, rgba(252, 22, 130, 0.40) -0.55%, rgba(252, 22, 130, 0.28) -0.54%, rgba(255, 168, 198, 0.00) 99.3%);
+    padding: 44px 30px;
+    &-title{
+      &-in{
+        font-size: 20px;
+        text-indent: 45px;
+      }
+    }
+    &-context{
+      margin-top: 30px;
+      span{
+        font-size: 16px;
+      }
+    }
+    &-content{
+      background: none;
+      margin-top: 70px;
+      padding: 0;
+      &-in{
+        flex-direction: column;
+        &>div{
+          width: 100%;
+          padding: 16px 12px;
+          &:not(:first-child){
+            margin-left: 0px;
+            margin-top: 70px;
+          }
+        }
+        &-l{
+          align-items: flex-end;
+          margin-top: -70px;
+          img{
+            width: 125px;
+            height: 125px;
+            margin-top: 5px;
+            border: 4px solid #fff;
+          }
+          span{
+            margin-left: 15px;
+            &:nth-of-type(1){
+              font-size: 20px;
+            }
+            &:nth-of-type(2){
+              font-size: 18px;
+            }
+          }
+        }
+        &-r{
+          margin-top: 10px;
+          font-size: 16px;
+        }
       }
     }
   }
