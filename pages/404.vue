0048bc39049a16bb0a5874004bfed1482bc73d49
Matilda Pescott <51993914+vikim540@users.noreply.github.com>
2024-07-24 14:51:28 +0800

    Update 404.vue

--- pages/404.vue
+++ pages/404.vue
@@ -16,16 +16,21 @@ useHead(() => ({
 <template>
   <div>
     <div class="flex flex-col items-center justify-center">
-      <Error :code="404" />
+     
     </div>
     <div style="width: 100%;margin-top:100px;">
-      <div class="img_err">
+      <div class="img_err" style="    text-align: center;">
       <img
         src="https://static.cmereye.com/imgs/2024/07/ce0c4dabdd6a15f8.png"
-        alt=""
+        alt="" style="    margin: auto;"
       />
-    </div>
-    <nuxt-link to="/" class="black">返回首頁</nuxt-link>
+         <nuxt-link to="/" class="cursor-pointer
+  border transition-color duration-300
+  focus:outline-none focus:ring-1 focus:ring-offset-1 focus:dark:ring-offset-gray-50 focus:dark:ring-gray-400 focus:ring-gray-600/[0.6] focus:ring-offset-gray-800/[0.6]
+  flex items-center justify-center font-semibold
+ text-white bg-primary-500 hover:bg-primary-400 border-primary-500 h-9 px-4 text-sm rounded" >返回首頁</nuxt-link>
+    </div> 
+   
       <serviceCard :isIndexShow="true" />
     </div>
   </div>
